890283ecf31df5041049bde480e1d7955dc05cc4
Kris Bogaert <kris.bogaert@ucll.be>
2020-10-03 17:14:50 +0200

    calling super

--- app/src/main/java/be/ucll/prog4/ucllpong/MainActivity.java
+++ app/src/main/java/be/ucll/prog4/ucllpong/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onResume() {
-        gv.resume();
         super.onResume();
+        gv.resume();
     }
 }
